49374e2a11ac8fb1fcf902c3f9a137bc69fdcdb3
Gadzet <saygitovg@gmail.com>
2022-12-22 20:07:48 +0300

    небольшие изменения, сжальтесь!

--- site/download/views.py
+++ site/download/views.py
@@ -1,5 +1,3 @@
-from django.http import HttpResponse
-
 from catalog.models import Book, BookChapter
 from django.shortcuts import get_object_or_404, render
 
@@ -7,13 +5,17 @@ from django.shortcuts import get_object_or_404, render
 def download_book_txt(request, book_id):
     template_name = 'download/txt.txt'
     book = get_object_or_404(Book, pk=book_id)
-    chapters = BookChapter.objects.for_user(
+    chapters, _ = BookChapter.objects.bought_required(
         request.user, book_id
     )
     context = {
         'book': book,
         'chapters': chapters
     }
-    response = render(request, template_name, context, content_type='text/plain; charset=utf-8')
-    response.headers['Content-Disposition'] = f'attachment; filename="{book_id}.txt"'
+    response = render(
+        request, template_name, context,
+        content_type='text/plain; charset=utf-8'
+        )
+    response.headers['Content-Disposition'] = \
+        f'attachment; filename="{book_id}.txt"'
     return response
