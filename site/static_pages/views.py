b9c9f92118211f1b5bf56264cfd4007e6eb9b698
Alex <sai071220052@gmail.com>
2022-12-22 19:49:35 +0300

    homepage added

--- site/static_pages/views.py
+++ site/static_pages/views.py
@@ -1,11 +1,20 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from catalog.models import Book
 
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
     template_name = 'static_pages/index.html'
+    queryset = Book.objects.enabled().order_by('-rating_count', '-avg_rating')
+    context_object_name = 'books'
     extra_context = {
-        'page_title': 'Главная'
-    }
+         'page_title': 'Главная'
+     }
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['should_open_filter'] = len(self.request.GET) != 0
+        return context
 
 
 class AboutPageView(TemplateView):
