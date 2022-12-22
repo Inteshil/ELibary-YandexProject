5defc3225e03190c405c766986b89b0922fd7d3d
Gadzet005 <88264036+Gadzet005@users.noreply.github.com>
2022-12-22 20:09:54 +0300

    Небольшие изменения, сжальтесь (#11)
    * Create base templates
    
    * Update setup.cfg
    
    * Create auth
    
    * Update auth, profile, user_details
    
    * Create catalog app
    
    * delete comments + create birthday date validator
    
    * Update chapters page
    
    * Add html editor
    
    * Create book and book chapter CRUD
    
    * Fix problem in admin
    
    * Update next and previous chapter
    
    * fix bugs
    
    * update migrations
    
    * update tags + create filters
    
    * fix problems
    
    * fix problems 2
    
    * go back to old commit + some updates
    
    * some changes
    
    * update filter
    
    * update comment manager
    
    * fix Exception
    
    * interim changes
    
    * Some changes
    
    * update payment
    
    * delete payment admin
    
    * Fix problems and some updates
    
    * Update README.md
    
    * set default book image
    
    * fix problems in book detail context
    
    * update book detail
    
    * небольшие изменения, сжальтесь!
    
    Co-authored-by: Alex <sai071220052@gmail.com>

--- site/download/urls.py
+++ site/download/urls.py
@@ -5,5 +5,8 @@ from download import views
 app_name = 'download'
 
 urlpatterns = [
-        path('book/<int:book_id>/txt', views.download_book_txt, name='download_book_txt'),
+        path(
+            'book/<int:book_id>/txt', views.download_book_txt,
+            name='download_book_txt'
+        ),
 ]
